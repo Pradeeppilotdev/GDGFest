e037037d38cec099c6802081dd71a7ea57fddafd
Pradeeppilot <chandrapradeep2005@outlook.com>
2025-01-30 00:41:50 +0530

    gdgfest

--- test.js
+++ test.js
@@ -5,8 +5,29 @@ let secBox = document.getElementById("sec-box");
 let endDate = new Date(2025,03,21,00,00);
 let endTime = endDate.getTime();
 
-function countdown{
+function countdown(){
   let todayDate = new Date();
   let todayTime = todayDate.getTime();
   let remainingTime = endTime - todayTime;
+  let oneMin = 60  *1000;
+  let oneHour = 60 * oneMin;
+  let oneDay = 24 * oneHour;
+  let addZeroes = (num) => (num < 10 ? `0${num}`:num);
+if (endTime < todayTime){
+    clearInterval(i);
+    document.querySelector(".countdown").innerHTML=`<h1>The Event Has Begun</h1>`;
+
 }
+else{
+    let daysLeft = Math.floor(remainingTime / oneDay);
+    let hrsLeft = Math.floor((remainingTime % oneDay)/oneHour);
+    let minsLeft = Math.floor((remainingTime % oneHour)/oneMin);
+    let secLeft = Math.floor((remainingTime % oneMin)/1000);
+    dayBox.textContent=addZeroes(daysLeft);
+    hrBox.textContent=addZeroes(hrsLeft);
+    minBox.textContent=addZeroes(minsLeft);
+    secBox.textContent=addZeroes(secLeft);
+}
+}
+let i = setInterval(countdown,1000);
+countdown();
