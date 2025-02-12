8b1357fe0c5664dce790d9cbc46735bceb864c21
Pradeeppilot <chandrapradeep2005@outlook.com>
2025-02-12 23:38:00 +0530

    gdgfest

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ let dayBox = document.getElementById("day-box");
 let hrBox = document.getElementById("hr-box");
 let minBox = document.getElementById("min-box");
 let secBox = document.getElementById("sec-box");
-let endDate = new Date(2025,03,21,00,00);
+let endDate = new Date(2025,02,21,00,00);
 let endTime = endDate.getTime();
 
 function countdown(){
